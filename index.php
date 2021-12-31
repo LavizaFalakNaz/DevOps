1aabf308b16c3b1a589bb241ed3e58b5620a53df
moiz290 <moiz29096@gmail.com>
2021-12-31 00:21:25 -0800

    .

--- index.php
+++ index.php
@@ -3,13 +3,3 @@
 ?>
     
 <?php
-<<<<<<< HEAD
-    echo "hello moiz";
-=======
-<<<<<<< Updated upstream
-    echo "hello world ";
-=======
-    echo "hello world";
->>>>>>> Stashed changes
->>>>>>> 99e17a43e54eca7aaadfd8a1472e3abc52d75d8d
-?>
