83ea11c6e8731507a91f166c554c279913914f0c
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2022-01-09 15:46:58 +0500

    PROD version

--- pages/home.php
+++ pages/home.php
@@ -6,8 +6,8 @@ if (isset($_SESSION['email']) && isset($_SESSION['id'])) {
     //we close php here so that whatever happens after it is in loop
 ?>
 
-<?php echo $_SESSION['email']; ?>
-<button onclick="location.href='logout.php'">Logout</button>
+    <?php echo $_SESSION['email']; ?>
+    <button onclick="location.href='logout.php'">Logout</button>
 
 <?php } else {
     header("Location: login.php?error=Please login to your account first.");
