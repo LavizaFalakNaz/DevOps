cd18a978406991f92f262278696b86a07839ca73
Moiz <95467236+moiz9999@users.noreply.github.com>
2022-01-11 15:50:05 +0500

    Layout

---
+++ layout/footer.php
@@ -0,0 +1,19 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="#" target="_blank">Devicks</a>.</strong> All rights reserved.
+  </footer>
+
+<!-- jQuery -->
+<script src="../frontend/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../frontend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- DataTables  & Plugins -->
+
+<!-- AdminLTE App -->
+<script src="../frontend/dist/js/adminlte.min.js"></script>
+
--- pages/home.php
+++ pages/home.php
@@ -6,10 +6,48 @@ if (isset($_SESSION['email']) && isset($_SESSION['id'])) {
     //we close php here so that whatever happens after it is in loop
 ?>
 
+<!--
     <?php echo $_SESSION['email']; ?>
     <button onclick="location.href='logout.php'">Logout</button>
-
+-->
 <?php } else {
     header("Location: login.php?error=Please login to your account first.");
 }
+
+
+$title = "Home Page";
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include_once "../layout/head.php";
+    ?>
+</head>
+<body class="sidebar-mini layout-fixed" style="height:auto">
+<div class="wrapper">
+
+<?php
+    include_once "../layout/navbar.php";
+    include_once "../layout/sidebar.php";
+
+?>
+
+
+
+
+
+
+
+
+</div>
+
+<?php
+    include_once "../layout/footer.php";
+?>
+
+
+</body>
+</html>
