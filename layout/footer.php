957bae4219b7e5702971b2f5ae3a983e55f5add4
Awais Ahmed <95467236+moiz9999@users.noreply.github.com>
2022-01-11 15:30:19 +0500

    layout

---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+
+<!-- jQuery -->
+<script src="../frontend/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../frontend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- DataTables  & Plugins -->
+
+<!-- AdminLTE App -->
+<script src="../frontend/dist/js/adminlte.min.js"></script>
+
