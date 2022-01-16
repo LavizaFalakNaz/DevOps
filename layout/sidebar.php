740aa39b46ecebc140da142a4d430830a6c5eb53
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2022-01-16 16:15:58 +0500

    page shift

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="../index.php" class="brand-link">
       <img src="../frontend/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Devicks &copy;</span>
     </a>
@@ -12,7 +12,7 @@
           <img src="../frontend/dist/img/avatar5.png" class="img-circle " alt="User Image">
         </div>
         <div class="info">
-          <a href="#"  class="d-block"><?php echo $asfun->getUsername($_SESSION["email"]); ?></a>
+          <a href="home.php"  class="d-block"><?php echo $asfun->getUsername($_SESSION["email"]); ?></a>
         </div>
       </div>
 
@@ -24,7 +24,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
           <li class="nav-item">
-            <a href="home.php" class="nav-link" >
+            <a href="sprints.php" class="nav-link" >
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
---
+++ pages/sprints.php
@@ -0,0 +1,78 @@
+<?php
+    include "../includes/auth.php";
+    $pageInfo = [
+        'title' => 'Dashboard'
+    ];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include_once "../layout/head.php";
+    ?>
+</head>
+<body class="sidebar-mini layout-fixed" style="height:auto">
+<div class="wrapper">
+
+<?php
+    include_once "../layout/navbar.php";
+    include_once "../layout/sidebar.php";
+
+?>
+
+
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0"><?php echo $pageInfo["title"]; ?></h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- /.col-md-6 -->
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="m-0">Featured</h5>
+              </div>
+              <div class="card-body">
+                <h6 class="card-title">Special title treatment</h6>
+
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+
+          </div>
+
+
+       
+          <!-- /.col-md-6 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+</div>
+<?php
+    include_once "../layout/footer.php";
+?>
+</body>
+</html>
