743af20299e169bb6bdc98a19dc06b7b7bfeeaff
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2022-01-16 17:28:24 +0500

    page setups

--- includes/main.class.php
+++ includes/main.class.php
@@ -1,45 +1,32 @@
 <?php
 
-class Main{
-
-    private $servername = "us-cdbr-east-05.cleardb.net";
-    private $username = "bcc77e1841a73a";
-    private $password = "dd32e024";
-    private $database = "heroku_7fce67cb249adf3";
-    public $dbcon;
-
-    public function __construct() {
-        $this->dbcon = new mysqli($this->servername, $this->username, $this->password, $this->database);
-      }
-
-public function auth(){
-  if (!(isset($_SESSION['email']) && isset($_SESSION['id']))) {
-    header("Location: login.php?error=Please login to your account first.");
+class Main
+{
+  private $servername = "us-cdbr-east-05.cleardb.net";
+  private $username = "bcc77e1841a73a";
+  private $password = "dd32e024";
+  private $database = "heroku_7fce67cb249adf3";
+  public $dbcon;
+  public function __construct()
+  {
+    $this->dbcon = new mysqli($this->servername, $this->username, $this->password, $this->database);
+  }
+  public function auth()
+  {
+    if (!(isset($_SESSION['email']) && isset($_SESSION['id']))) {
+      header("Location: login.php?error=Please login to your account first.");
+    }
+  }
+  public function getUsername($email)
+  {
+    $res = $this->dbcon->query("select * from registrations where email='$email'");
+    $row = $res->fetch_array();
+    return $row["name"];
+  }
+  public function getProjectName($id)
+  {
+    $res = $this->dbcon->query("select * from projects where id='$id'");
+    $row = $res->fetch_array();
+    return $row["name"];
+  }
 }
-}
-
-
-      public function getUsername($email){
-        $res = $this->dbcon->query("select * from registrations where email='$email'");    
-        $row = $res->fetch_array();
-        return $row["name"];
-      
-      }
-
-
-      public function getProjectName($id){
-        $res = $this->dbcon->query("select * from projects where id='$id'");    
-        $row = $res->fetch_array();
-        return $row["name"];
-      }
-
-}
-
-
-
-
-
-
-
-
-?>
