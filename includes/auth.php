51d88df9fb11dcf1e2a15f0e8811abd92b7d8495
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2022-03-02 22:25:10 +0500

    Release 0.0.1

--- includes/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-include "../includes/main.class.php";
-$asfun = new Main();
-$asfun->auth();
-?>
-
--- operations/readme.md
+++
@@ -1,2 +0,0 @@
-Add Extra  files here and refer wherever required. 
-this folder willl be hidden in the prod directory
