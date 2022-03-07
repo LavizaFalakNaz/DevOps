3be93873223c323f50a1b55ab865ba44c3a92f05
Laviza Falak Naz <lavizaniazi2001@gmail.com>
2022-03-07 16:03:19 +0500

    update follow-redirect issue

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
-if (isset($_SESSION['email']) && isset($_SESSION['id'])) {
-
+  session_start();
+  if (isset($_SESSION['email']) && isset($_SESSION['id'])) {
 ?>
 
-<?php } else {
+<?php 
+  } else {
   header("Location: login.php?error=Please enter your email and password to start.");
   exit();
 }
